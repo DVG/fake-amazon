e11c4cf0a93e57ee56b2140ff2b6da1310bdcfd9
Bradley Temple <btemple@covermymeds.com>
2018-02-17 22:53:14 -0500

    Ready for stage 1 exercise

---
+++ script/free-shipping-on-orders-over-35-dollars.rb
@@ -0,0 +1,10 @@
+require_relative '../config/setup-test'
+
+
+visit "/"
+
+# Write your test here
+
+# Your script will pause on this line and let you play
+# with the site in the terminal
+binding.pry
